3e3e823591821622aa079cef56976a95866fd411
Tiago Nunes (BPI) <tiago.santos.nunes@bancobpi.pt>
2018-01-26 16:58:25 +0000

    Change to iOS available directive. v 3.4.0

--- src/ios/TouchID.m
+++ src/ios/TouchID.m
@@ -22,6 +22,12 @@
 #include <sys/sysctl.h>
 #import <Cordova/CDV.h>
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 @implementation TouchID
 
 - (void)isAvailable:(CDVInvokedUrlCommand*)command{
@@ -35,7 +41,9 @@
     
     if ([self.laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil]) {
         NSString *biometryType = @"";
-        if (@available(iOS 11.0, *)) {
+
+        if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"11"))
+        {
             if (self.laContext.biometryType == LABiometryTypeFaceID) {
                 biometryType = @"face";
             }
