fb68444da0f7b8faf28e19b6fcdeb42900d63d02
adnan.moatadidbillah@saphirsystems.com <adnan.moatadidbillah>
2020-03-02 18:41:28 +0100

    save

--- src/ios/TouchID.m
+++ src/ios/TouchID.m
@@ -24,36 +24,66 @@
 
 @implementation TouchID
 
-- (void)isAvailable:(CDVInvokedUrlCommand*)command{
-    if (NSClassFromString(@"LAContext") == NULL) {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        return;
-    }
+// - (void)isAvailable:(CDVInvokedUrlCommand*)command{
+//     if (NSClassFromString(@"LAContext") == NULL) {
+//         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//         return;
+//     }
     
-    self.laContext = [[LAContext alloc] init];
+//     self.laContext = [[LAContext alloc] init];
     
-    if ([self.laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil]) {
-        NSString *biometryType = @"";
-        if (@available(iOS 11.0, *)) {
-            if (self.laContext.biometryType == LABiometryTypeFaceID) {
-                biometryType = @"face";
-            }
-            else {
-                biometryType = @"touch";
-            }
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:biometryType];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        }
-        else {
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"touch"];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        }
-    }
-    else {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"Touch ID not available"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
+//     if ([self.laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil]) {
+//         NSString *biometryType = @"";
+//         if (@available(iOS 11.0, *)) {
+//             if (self.laContext.biometryType == LABiometryTypeFaceID) {
+//                 biometryType = @"face";
+//             }
+//             else {
+//                 biometryType = @"touch";
+//             }
+//             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:biometryType];
+//             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//         }
+//         else {
+//             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"touch"];
+//             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//         }
+//     }
+//     else {
+//         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"Touch ID not available"];
+//         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//     }
+// }
+
+
+- (void) isAvailable:(CDVInvokedUrlCommand*)command {
+    
+  LAContext *context = [[LAContext alloc] init];
+  
+  if (NSClassFromString(@"LAContext") == NULL) {
+      [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR] callbackId:command.callbackId];
+      return;
+  }
+  
+  [self.commandDelegate runInBackground:^{
+      NSError *error = nil;
+      LAContext *laContext = [[LAContext alloc] init];
+      if ([laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+          NSString *biometryType = @"touch";
+          if (@available(iOS 11.0, *)) {
+              if (laContext.biometryType == LABiometryTypeFaceID) {
+                  biometryType = @"face";
+              }
+          }
+          [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:biometryType]
+                                      callbackId:command.callbackId];
+      } else {
+          NSArray *errorKeys = @[@"code", @"localizedDescription"];
+          [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:[error dictionaryWithValuesForKeys:errorKeys]]
+                                      callbackId:command.callbackId];
+      }
+  }];
 }
 
 - (void)setLocale:(CDVInvokedUrlCommand*)command{
