127450cd1ec01ed3385412527c4718503f33243b
adnan.moatadidbillah@saphirsystems.com <adnan.moatadidbillah>
2021-03-16 12:44:28 +0100

    Fix illegal state exception

--- src/android/FingerprintAuthAux.java
+++ src/android/FingerprintAuthAux.java
@@ -501,8 +501,14 @@ public class FingerprintAuthAux {
                     mFragment.setCancelable(false);
                     // Show the fingerprint dialog. The user has the option to use the fingerprint with
                     // crypto, or you can fall back to using a server-side verified password.
-                    mFragment.setCryptoObject(new FingerprintManager.CryptoObject(mCipher));
-                    mFragment.show(cordova.getActivity().getFragmentManager(), DIALOG_FRAGMENT_TAG);
+                    mFragment.setCryptoObject(new FingerprintManager.CryptoObject(mCipher)); 
+                    try {
+                        mFragment.show(cordova.getActivity().getFragmentManager(), DIALOG_FRAGMENT_TAG);
+                    } catch (IllegalStateException ignored) {
+                        // Return error in callback so we can handle this in implementation
+                        mPluginResult = new PluginResult(PluginResult.Status.ERROR, "Failed to open fingerprint dialog");
+                        mCallbackContext.sendPluginResult(mPluginResult);
+                    }
                 } else {
                     mPluginResult = new PluginResult(PluginResult.Status.ERROR, "Failed to init Cipher");
                     mCallbackContext.sendPluginResult(mPluginResult);
